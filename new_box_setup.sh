5817af06533e89d756fe93a46e46b9e28e406ec8
bbarwick-SIOS <brett.barwick@us.sios.com>
2018-08-19 15:48:22 -0400

    added new_box_setup

---
+++ new_box_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+add-apt-repository ppa:openjdk-r/ppa
+apt-get update
+apt-get upgrade
+apt-get install build-essential
+apt-get install maven
+apt-get install openjdk-7-jdk
+apt-get install libexpat-dev
+apt-get install libssl-dev
+cpan XML::Parser
+cpan XML::DOM
+cpan JSON
+cpan Crypt::OpenSSL::RSA
+apt-get install dpkg-sig
+apt-get install reprepr
